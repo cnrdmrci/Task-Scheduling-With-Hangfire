200bdf639f33baabb42f0c320ca2455928102b69
cnrdmrci <canerdemirci.mail@gmail.com>
2020-04-02 00:37:56 +0300

    Yeni örnek classlar eklendi

---
+++ Database.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+
+namespace TaskSchedulingWithHangfire
+{
+    public class Database
+    {
+        [AutomaticRetry(Attempts = 3)]
+        public void BackUp()
+        {
+            Console.WriteLine("Database BackUp Run");
+        }
+    }
+}
---
+++ Example.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskSchedulingWithHangfire
+{
+    public class Example
+    {
+        public void Run()
+        {
+            Console.WriteLine("Example, Run");
+        }
+    }
+}
