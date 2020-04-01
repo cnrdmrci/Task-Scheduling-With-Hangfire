9241a8bacf7201ab9b4fb6bfb1c67f50e964db10
cnrdmrci <canerdemirci.mail@gmail.com>
2020-04-01 23:09:01 +0300

    project created

---
+++ Controllers/HangfireController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TaskSchedulingWithHangfire.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HangfireController : ControllerBase
+    {
+
+        private readonly ILogger<HangfireController> _logger;
+
+        public HangfireController(ILogger<HangfireController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
