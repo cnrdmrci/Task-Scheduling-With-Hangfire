b9cb29b383e9e10e8b1d7733dca31292b21e6314
cnrdmrci <canerdemirci.mail@gmail.com>
2020-04-02 00:38:24 +0300

    Dashboard girişi için yetki kontrol classı eklendi.

---
+++ HangfireDashboardAuthorizationFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Hangfire.Dashboard;
+
+namespace TaskSchedulingWithHangfire
+{
+    public class HangfireDashboardAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            var httpContext = context.GetHttpContext();
+            var userRole = httpContext.User.FindFirst(ClaimTypes.Role)?.Value;
+            //return userRole == "roleNameOrId";  
+            return true;
+        }
+    }
+}
